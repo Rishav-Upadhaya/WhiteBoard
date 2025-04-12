cdf440a0d9db872c5fc5934654ebacbcfee2da12
Rishav-Upadhaya <rishavupadhaya266@gmail.com>
2025-04-12 23:53:10 +0545

    Updated the flow with some comments to learn

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import whiteboard_view
+from .views import whiteboard_view, home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('<str:room_name>/', whiteboard_view, name='whiteboard'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def home_view(request):
+    return render(request, "board/home.html")
+
 def whiteboard_view(request, room_name):
     return render(request, 'board/whiteboard.html', {'room_name':room_name})
